e0677d4d2a0f591cc4be82ac68da0fc559d5c78b
shlok <shlok@pop-os.localdomain>
2023-07-12 13:48:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
@@ -6,18 +5,21 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
+import { GlobalProvider } from './context/GlobalState';
+
+import './App.css';
+
 function App() {
   return (
-    <div className="">
+    <GlobalProvider>
       <Header />
-      <div className='container'>
-      <Balance />
-      <IncomeExpenses />
-      <TransactionList/>
-      <AddTransaction/>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
-      
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>
-        Expense Tracker
-    </div>
+    <h2>
+      Expense Tracker
+    </h2>
   )
 }
